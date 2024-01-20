a6dd047136096165469b6c4aa6c6aacc530387b3
Nicolas Emeriau <nicolas.emeriau@toolsvet.fr>
2024-01-20 12:24:28 +0100

    installation de jwt

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Entity\User;
+use App\Repository\ClientRepository;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ProductController extends AbstractController
+{
+    #[Route(
+        path: '/api/products',
+        name: 'api_products',
+        methods: ['GET']
+    )]
+    public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $productList = $productRepository->findAll();
+        $jsonProductList = $serializer->serialize($productList, 'json', ['groups' => 'getProducts']);
+        return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route(
+        path: '/api/products/{id}',
+        name: 'api_detailProduct',
+        methods: ['GET']
+    )]
+    public function getDetailProduct(Product $product, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonProduct = $serializer->serialize($product, 'json', ['groups' => 'getProducts']);
+        return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
+    }
+
+
+    #[Route(
+        path: '/api/products',
+        name: 'api_create_product',
+        methods: ['POST']
+    )]
+    #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour créer un produit')]
+    public function createProduct(
+        Request $request,
+        SerializerInterface $serializer,
+        EntityManagerInterface $manager,
+        UrlGeneratorInterface $urlGenerator,
+        ValidatorInterface $validator
+    ): JsonResponse
+    {
+        $product = $serializer->deserialize($request->getContent(), Product::class, 'json');
+
+        //On vérifie les erreurs
+        $errors = $validator->validate($product);
+        if ($errors->count() > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
+
+        $manager->persist($product);
+        $manager->flush();
+
+        $jsonProduct = $serializer->serialize($product, 'json', ['groups' => 'getProducts']);
+
+        $location = $urlGenerator->generate('api_detailProduct', ['id' => $product->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonProduct, Response::HTTP_CREATED, ["Location" => $location], true);
+    }
+
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Entity\User;
+use App\Repository\ClientRepository;
+use App\Repository\ProductRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class UserController extends AbstractController
+{
+    #[Route(
+        path: '/api/users',
+        name: 'api_users',
+        methods: ['GET']
+    )]
+    public function getAllUsers(UserRepository $userRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $userList = $userRepository->findAll();
+        $jsonUserList = $serializer->serialize($userList, 'json', ['groups' => 'getUsers']);
+        return new JsonResponse($jsonUserList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route(
+        path: '/api/users',
+        name: 'api_create_user',
+        methods: ['POST']
+    )]
+    public function createUser(
+        Request $request,
+        SerializerInterface $serializer,
+        EntityManagerInterface $manager,
+        UrlGeneratorInterface $urlGenerator,
+        ClientRepository $clientRepository,
+        ValidatorInterface $validator
+    ): JsonResponse
+    {
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+        //On vérifie les erreurs
+        $errors = $validator->validate($user);
+        if ($errors->count() > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
+
+        $manager->persist($user);
+        $manager->flush();
+
+        $content = $request->toArray();
+        $idClient = $content['idClient'] ?? -1;
+
+        $user->setClient($clientRepository->find($idClient));
+
+        $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getUsers']);
+
+        $location = $urlGenerator->generate('api_detailUser', ['id' => $user->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED, ["Location" => $location], true);
+    }
+
+    #[Route(
+        path: '/api/users/{id}',
+        name: 'api_detailUser',
+        methods: ['GET']
+    )]
+    public function getDetailUser(User $user, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getUsers']);
+        return new JsonResponse($jsonUser, Response::HTTP_OK, [], true);
+    }
+
+    #[Route(
+        path: '/api/users/{id}',
+        name: 'api_updateUser',
+        methods: ['PUT']
+    )]
+    public function updateUser(
+        Request $request,
+        User $currentUser,
+        SerializerInterface $serializer,
+        EntityManagerInterface $manager,
+        ClientRepository $clientRepository
+    ): JsonResponse
+    {
+        $updatedUser = $serializer->deserialize($request->getContent(), User::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $currentUser]);
+
+        $content = $request->toArray();
+        $idClient = $content['idClient'] ?? -1;
+
+        $updatedUser->setClient($clientRepository->find($idClient));
+
+        $manager->persist($updatedUser);
+        $manager->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route(
+        path: '/api/users/{id}',
+        name: 'api_deleteUser',
+        methods: ['DELETE']
+    )]
+    public function deleteUser(User $user, EntityManagerInterface $manager): JsonResponse
+    {
+        $manager->remove($user);
+        $manager->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+}
